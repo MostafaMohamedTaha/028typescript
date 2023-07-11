35244cded176b25db9e24067b39c5ca3ddbdc84c
MostafaMohamedTaha <MostafaMohamedTaha.GitHup@gmail.com>
2023-07-11 07:49:57 +0200

    1

---
+++ dist/A.js
@@ -0,0 +1,16 @@
+"use strict";
+//tsc a.ts -npm i -g typescript
+//(((((((((((((((((1)))))))))))))))))
+// let aa: string='aa'
+// let aa:boolean=true
+// let ss:number=232322.2
+// console.log(ss.toFixed()) //int
+// console.log(a.toLowerCase())
+// export{}
+//(((((((((((((((((2)))))))))))))))))
+//function
+// let a=(num:number)=>num+2
+// console.log(a(2)) //must be number
+// export{}
+//(((((((((((((((((3)))))))))))))))))
+//# sourceMappingURL=A.js.map
